b0e23fe9ca09b3d26e4351ce7e461fa9526eabf5
abdishukri <abdishukrimohamed105@gmail.com>
2023-02-10 19:22:27 +0300

    passed all tests

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+require "pry"
+class Anagram
+
+      attr_accessor :word
+    def initialize(word)
+        @word = word
+    end
+
+    def match(arr_anagrams) 
+        arr_anagrams.filter do |anagram|
+           anagram.chars.sort == @word.chars.sort
+        end
+        
+    end
+end
+
+listen = Anagram.new("listen")
+
+binding.pry
